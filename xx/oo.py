47f608c37b11b7fe48a9daa7c18c63282e2b000a
Your Name <xxx@live.com>
2019-07-02 14:06:06 +0800

    this is a test do not worry

---
+++ xx/oo.py
@@ -0,0 +1,7 @@
+name = '小黑'
+
+class Person():
+    def __init__(self,name):
+        self.name = name
+    def der(self):
+        print("woshi {}".format(self.name))
---
+++ xx/test.py
@@ -0,0 +1,14 @@
+# from xx import oo
+# print(oo.name)
+# s = 'xx.oo'
+#
+# import importlib
+# a = importlib.import_module('xx.oo')
+# print(a.name)
+#
+# # 反射的方式去拿 类
+# a_class = getattr(a,'Person')
+# p = a_class('士大夫')
+# p.der()
+
+
